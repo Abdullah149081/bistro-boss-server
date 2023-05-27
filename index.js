31b2ccb0d09da884f8659c38376af8ff389f0996
Abdullah149081 <masudmd425@gmail.com>
2023-05-28 00:04:53 +0600

    reviews api done

--- index.js
+++ index.js
@@ -33,9 +33,14 @@ async function run() {
     });
 
     const bossCollection = client.db("bistroBossDB").collection("menu");
+    const reviewsCollection = client.db("bistroBossDB").collection("reviews");
 
     app.get("/menu", async (req, res) => {
       const result = await bossCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
 
