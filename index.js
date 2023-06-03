cd9ab0ee0f39816deac5bc71879788d97397244c
Abdullah149081 <masudmd425@gmail.com>
2023-06-04 00:22:14 +0600

    carts api test

--- index.js
+++ index.js
@@ -113,13 +113,17 @@ async function run() {
     });
 
     // carts api
-    app.get("/carts", async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        res.send([]);
-      }
-      const query = { email: email };
+    app.get("/carts", verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
 
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "forbidden access" });
+      }
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
       const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
