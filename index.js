9b05752521c47f6269d8325cbdca8112df87c722
Abdullah149081 <masudmd425@gmail.com>
2023-06-03 20:56:57 +0600

    patch api for user admin

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
 
     // users api
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -47,6 +52,18 @@ async function run() {
         return res.send({ message: "User Already Create" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateUser = {
+        $set: {
+          roll: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateUser);
       res.send(result);
     });
 
