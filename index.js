5a13b3ce7b06d8bf9ad590793c25d6fc30e1d6da
Abdullah149081 <masudmd425@gmail.com>
2023-06-05 09:57:55 +0600

    menu delete api is done

--- index.js
+++ index.js
@@ -133,6 +133,12 @@ async function run() {
       const result = await bossCollection.insertOne(newItem);
       res.send(result);
     });
+    app.delete("/menu/:id", verifyJwt, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bossCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // reviews api
     app.get("/reviews", async (req, res) => {
